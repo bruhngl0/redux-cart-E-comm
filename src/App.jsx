bfb219648669e3e11f0b3913913ae56bea337021
bruh.ngl0 <adityashrm500@gmail.com>
2023-05-16 07:53:33 +0530

    part one

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import{Toaster} from "react-hot-toast"
 
+import "./styles/app.scss"
+
+import Header from './components/Header'
+import Home from './components/Home'
 
 function App() {
   
 
   return (
-   <h1>lol</h1>
+
+    
+
+   <Router>
+      <Header />
+      <Routes>
+        <Route path= "/" element= {<Home />}/>
+      </Routes>
+      <Toaster />
+   </Router> 
   )
 }
 
